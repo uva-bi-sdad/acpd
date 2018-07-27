79634997b1b8702cec241be1e92d41ec194f4982
Hannah Brinkley <hsbb8@vt.edu>
2018-07-27 17:58:10 +0000

    code for the maps for acpd dashboard

--- src/hannah_acpd_data_cleaning.R
+++ src/hannah_acpd_data_cleaning.R
@@ -1,20 +1,55 @@
 library(lubridate)
-data$firstReportDtm <- as.Date(data$firstReportDtm)
-data$lastReportDtm <- as.Date(data$lastReportDtm)
+library(sdalr)
+pacman::p_load(docstring, sdalr, DBI, dplyr, data.table, dtplyr)
 
-data <- read.csv("~/acpd/data/TEST_acpd_open_data.csv")
-data$year <- NA
-data$month <- NA
-data$day_of_week <- NA
-data$hour <- NA
-
-
-for (i in 1:nrow(data))
-{
-  data$year[i] <- as.character(year(data$firstReportDtm[i]))
-  data$month[i] <- as.character(month(data$firstReportDtm[i]))
-  data$day_of_week[i] <- as.character(wday(data$firstReportDtm[i], label = TRUE, abbr = FALSE))
-  
+db_data = function() {
+  conn = con_db(dbname = 'acpd',
+                pass = get_my_password())
+  output = dbReadTable(conn = conn,
+                       name = 'clean_acpd_data') %>%
+    data.table()
+  on.exit(dbDisconnect(conn = conn))
+  return(value = output)
 }
 
-data2 <- read.csv("~/acpd/data/filteredby_location_peaktime.csv")
+test <- db_data()
+
+upload_file = function(data) {
+  conn = con_db(dbname = 'jbsc',
+                pass = get_my_password())
+  dbWriteTable(conn = conn,
+               name = 'clean_acpd_data',
+               value = data,
+               row.names = FALSE,
+               overwrite = TRUE)
+  on.exit(expr = dbDisconnect(conn = conn))
+}
+
+
+acpd_data <- db_data()
+
+
+# data$firstReportDtm <- as.Date(data$firstReportDtm)
+# data$lastReportDtm <- as.Date(data$lastReportDtm)
+# acpd_data$start <- as.Date(acpd_data$start)
+
+acpd_data$year <- NA
+acpd_data$month <- NA
+acpd_data$day_of_week <- NA
+acpd_data$hour <- NA
+
+for (i in 1:nrow(acpd_data)) 
+{
+  acpd_data$year[i] <- as.character(year(acpd_data$start[i]))
+  acpd_data$month[i] <- as.character(month(acpd_data$start[i]))
+  acpd_data$day_of_week[i] <- as.character(lubridate::wday(acpd_data$start[i], label = TRUE, abbr = FALSE))
+  acpd_data$hour[i] <- hour(acpd_data$start[i])
+  print(i)
+}
+
+upload_file(data = test)
+
+write.csv(test, "~/acpd/data/acpd/final/acpd_clean_data.csv")
+
+  
+
