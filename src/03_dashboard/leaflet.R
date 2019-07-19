61f7be2dfd6f92c3bfd4e2b10b9848a7ca3bf6c2
Aaron D. Schroeder <dads2busy@gmail.com>
2019-07-19 15:45:30 +0000

    remove browser

--- src/03_dashboard/leaflet.R
+++ src/03_dashboard/leaflet.R
@@ -155,7 +155,7 @@ make_crime_map <- function(crime_type = "Drunkenness") {
 
   if (!exists("pnts_sf")) pnts_sf <- readRDS("map_pnts_sf.RDS")
   map_pnts_sf <- dplyr::filter(pnts_sf, crime_category == crime_tp)
-browser()
+
   if (!exists("pnts_2_sf")) pnts_2_sf <- readRDS("map_pnts_2_sf.RDS")
   map_pnts_2_sf <- pnts_2_sf
 
